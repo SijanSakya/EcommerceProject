468f1dc3571dc1c531f81bef219f2c838108142d
SijanSakya <73643998+SijanSakya@users.noreply.github.com>
2021-09-06 21:50:11 +0545

    Update user.php

--- user.php
+++ user.php
@@ -4,10 +4,10 @@ class user
 
 	public $user_id;
 	public $username;
-    public $email;
+        public $email;
 	public $password;
-    public $address;
-    public $phone_number;
+        public $address;
+        public $phone_number;
 
 
 
@@ -16,7 +16,10 @@ class user
 
 		$this->user_id = mysqli_real_escape_string($GLOBALS['conn'], $user_id);
 		$this->username = mysqli_real_escape_string($GLOBALS['conn'], $username);
+		$this->email = mysqli_real_escape_string($GLOBALS['conn'], $email);
 		$this->password = mysqli_real_escape_string($GLOBALS['conn'], $password);
+		$this->address = mysqli_real_escape_string($GLOBALS['conn'], $address);
+		$this->phone_number = mysqli_real_escape_string($GLOBALS['conn'], $phone_number);
 	}
 
 	public function toString()
@@ -27,7 +30,10 @@ class user
 	//user
 	public function create_user()
     {
-		$sql = "INSERT INTO `user` (`username`,`email`,`password`,`address`,,`phone_number`) VALUES ( '$objdes->username','$objdes->email','$objdes->password','$objdes->address','$this->phone_number')";
+		$sql = "INSERT INTO `user` (`username`,`email`,`password`,`address`,`phone_number`) 
+		VALUES ( '$this->username','$this->email','$this->password','$this->address','$this->phone_number')";
+		// print_r($sql);
+		// die;
 		$result = mysqli_query($GLOBALS['conn'], $sql);
 		if (!$result) {
 			echo "Error" . mysqli_error($GLOBALS['conn']) . "<br>";
@@ -38,14 +44,22 @@ class user
 
 	public function read_user()
 	{
-		// username, password
-		$sql = "SELECT * FROM `user` WHERE `username` = '$this->username' AND `password` = '$this->password'";
+		
+		$sql = "SELECT * FROM `user` WHERE
+		 `email` = '$this->email' AND `password` = '$this->password' ";
 		$result = mysqli_query($GLOBALS['conn'], $sql);
 		
-		if (!$result) {
-			echo "Error" . mysqli_error($GLOBALS['conn']) . "<br>";
-		}
-	}
+		// print_r($sql);
+		// die;
+		if ($result) {
+           
+                return true;
+            } else {
+            }
+        } else {
+            return false;
+        }
+    }
 	
 
 
